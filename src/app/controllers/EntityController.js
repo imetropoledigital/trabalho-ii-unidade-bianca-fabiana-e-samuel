9d20064bf029708b9306891645245ce7ab7b78d5
Bianca Miranda <miranda.bianca41@gmail.com>
2025-01-27 00:14:49 -0300

    Generalização da Entidade

---
+++ src/app/controllers/EntityController.js
@@ -0,0 +1,89 @@
+import mongoose from 'mongoose';
+
+class EntityController {
+    // Função para obter dinamicamente a coleção com base no nome da entidade
+    async getCollection(entityName){
+        return mongoose.model(entityName, new mongoose.Schema({}, { strict: false }), entityName);
+    };
+
+    // Criar nova entidade na coleção especificada
+    async createEntity (req, res){
+        const { entityName, data } = req.body;
+
+        if (!entityName || !data) {
+            return res.status(400).json({ message: 'Nome da entidade e dados são obrigatórios' });
+        }
+
+        try {
+            const Model = getCollection(entityName);
+            const newEntity = new Model(data);
+            await newEntity.save();
+            res.status(201).json({ message: 'Entidade criada com sucesso', newEntity });
+        } catch (error) {
+            res.status(500).json({ message: 'Erro ao salvar entidade', error });
+        }
+    };
+
+    // Obter todas as entidades de uma coleção específica
+    async getEntities (req, res){
+        const { entityName } = req.params;
+
+        try {
+            const Model = getCollection(entityName);
+            const entities = await Model.find({});
+            res.status(200).json(entities);
+        } catch (error) {
+            res.status(500).json({ message: 'Erro ao buscar entidades', error });
+        }
+    };
+
+    async findById(req, res) {
+        try {
+          const { entityName, id } = req.params;
+          const Model = getCollection(entityName);
+          const entity = await Model.findById(id);
+          if (entity) {
+            res.status(200).send(entity);
+          } else {
+            res.status(404).send({ error: 'Entidade não encontrada' });
+          }
+        } catch (error) {
+            res.status(500).send({ error: error.message });
+        }
+      }
+
+    // Deletar entidade por ID
+    async deleteByID (req, res){
+        const { entityName, id } = req.params;
+
+        try {
+            const Model = getCollection(entityName);
+            const result = await Model.findByIdAndDelete(id);
+            if (result) {
+                res.status(200).json({ message: 'Entidade removida com sucesso' });
+            }else{
+                return res.status(404).json({ message: 'Entidade não encontrada' });
+            }
+        } catch (error) {
+            res.status(500).send({ error: error.message });
+        }
+    };
+
+    async findByIdAndUpdate(req, res) {
+        const id = req.params.id;
+        const data = req.body;
+    
+        try {
+          const Model = getCollection(entityName);
+          const entity = await Model.findByIdAndUpdate(id, data, { new: true, runValidators: true });
+          if (entity) {
+            res.status(204).end();
+          } else {
+            res.status(404).send({ error: 'Usuário não encontrado' });
+          }
+        } catch (error) {
+          res.status(500).send({ error: error.message });
+        }
+    }
+}
+export default new EntityController();
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,11 +1,11 @@
 import Router from 'express';
-import UserController from '../app/controllers/UserController.js';
+import EntityController from '../app/controllers/EntityController.js';
 
 const router = Router();
 
-router.post('/users', UserController.createUser);
-router.get('/users', UserController.findAll);
-router.get('/users/:id', UserController.findById);
-router.put('/users/:id', UserController.findByIdAndUpdate);
+router.post('/entities', EntityController.createEntity);
+router.get('/entities/users', EntityController.getEntities);
+router.get('/entities/users/:id', EntityController.findById);
+router.put('/entities/users/:id', EntityController.findByIdAndUpdate);
 
 export default router;
